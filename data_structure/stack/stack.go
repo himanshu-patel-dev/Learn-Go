bcdeedd81424a5f9f95d12cd0ade90e94a6e62b3
Himanshu Patel <py.himanshu.patel@gmail.com>
2022-10-07 00:30:37 +0530

    Stack

---
+++ data_structure/stack/stack.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Stack struct {
+	items []int
+}
+
+func (s *Stack) Push(value int) {
+	s.items = append(s.items, value)
+}
+
+func (s *Stack) Pop() int {
+	l := len(s.items)
+	if l < 0 {
+		return -1
+	}
+	result := s.items[l-1]
+	s.items = s.items[:l-1]
+	return result
+}
+
+func main() {
+	myStack := &Stack{}
+	myStack.Push(10)
+	myStack.Push(20)
+	myStack.Push(30)
+	fmt.Println(myStack)
+
+	fmt.Println(myStack.Pop())
+	fmt.Println(myStack.Pop())
+	fmt.Println(myStack.Pop())
+	fmt.Println(myStack)
+}
+
+/*
+&{[10 20 30]}
+30
+20
+10
+&{[]}
+*/
