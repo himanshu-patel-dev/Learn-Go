0db8abe7d10042e1d9f44ee6f1d2cbbb4b30ad6a
Himanshu Patel <py.himanshu.patel@gmail.com>
2022-10-07 08:49:30 +0530

    BST

---
+++ data_structure/bst/bst.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+// Node to BST
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+// Insert node in BST
+func (n *Node) Insert(value int) *Node {
+	if n == nil {
+		return &Node{data: value}
+	}
+
+	if n.data > value {
+		n.left = n.left.Insert(value)
+	} else if n.data < value {
+		n.right = n.right.Insert(value)
+	} else {
+
+	}
+	return n
+}
+
+// Search for value in BST
+func (n *Node) Search(value int) bool {
+	if n == nil {
+		return false
+	}
+	if n.data == value {
+		return true
+	} else if n.data < value {
+		return n.right.Search(value)
+	} else {
+		return n.left.Search(value)
+	}
+}
+
+func main() {
+	tree := &Node{data: 100}
+	tree.Insert(50)
+	tree.Insert(150)
+	fmt.Println(tree.left.data, tree.data, tree.right.data)
+	fmt.Println(tree.Search(150))
+	fmt.Println(tree.Search(151))
+}
+
+/*
+50 100 150
+&{150 <nil> <nil>}
+<nil>
+*/
