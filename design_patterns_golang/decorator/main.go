4a1e78c654d5658fa0bed55939014d7def24e319
himanshu.patel <py.himanshu.patel@gmail.com>
2023-09-28 11:23:42 +0530

    Decorator Pattern - fixups

--- design_patterns_golang/decorator/main.go
+++ design_patterns_golang/decorator/main.go
@@ -13,6 +13,9 @@ Decorator is a structural pattern that allows adding new behaviors
 to objects dynamically by placing them inside special wrapper objects,
 called decorators.
 
+Thus the calling method on decorators is exactaly same
+as the method we need to call on base object
+
 */
 
 func main() {
