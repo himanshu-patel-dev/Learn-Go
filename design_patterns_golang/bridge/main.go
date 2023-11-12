728e4c5a29715578da7df522460d2bf8779b00ab
himanshu.patel <py.himanshu.patel@gmail.com>
2023-11-13 03:48:07 +0530

    bridge update

--- design_patterns_golang/bridge/main.go
+++ design_patterns_golang/bridge/main.go
@@ -4,9 +4,66 @@ import "fmt"
 
 /*
 
+------------- DEFINITION: BRIDGE PATTERN
+
 Bridge is a structural design pattern that divides business logic
 or huge class into separate class hierarchies that can be developed
 independently.
+
+------------- PROBLEM
+Two shapes : Circle and Square
+Two colors: Red and Blue
+
+If we make each class for one combination (blue circle, red circle, blue sq, red sq)
+then these are 4 combination/classes.
+
+Rather we can use composition over inheritance
+Circle (refer)-> blue/red class
+Square (refer)-> blue/red class	
+
+------------- APPLICATION
+- Use the Bridge pattern when you want to divide and organize a monolithic class that 
+has several variants of some functionality (for example, if the class can work with 
+various database servers).
+
+- Use the pattern when you need to extend a class in several orthogonal 
+(independent) dimensions.
+
+- Use the Bridge if you need to be able to switch implementations at runtime.
+
+------------- IMPLEMENTATION
+
+Abstraction (also called interface) is a high-level control layer for some entity. 
+This layer isn’t supposed to do any real work on its own. It should delegate the 
+work to the implementation layer (also called platform).
+
+Abstraction: the GUI layer of the app.
+Implementation: the operating systems’ APIs.
+
+- In below example mac/pc class act as abstraction and printer class act as implementation
+
+- Identify the orthogonal dimensions in your classes. These independent concepts 
+could be: abstraction/platform, domain/infrastructure, 
+front-end/back-end, or interface/implementation.
+
+- See what operations the client needs and define them in the base abstraction class.
+
+- Determine the operations available on all platforms. Declare the ones that the 
+abstraction needs in the general implementation interface.
+
+- For all platforms in your domain create concrete implementation classes, 
+but make sure they all follow the implementation interface.
+
+- Inside the abstraction class, add a reference field for the implementation type. 
+The abstraction delegates most of the work to the implementation object that’s 
+referenced in that field.
+
+- If you have several variants of high-level logic, create refined abstractions 
+for each variant by extending the base abstraction class.
+
+- The client code should pass an implementation object to the abstraction’s constructor 
+to associate one with the other. After that, the client can forget about the 
+implementation and work only with the abstraction object.
 
 */
 
