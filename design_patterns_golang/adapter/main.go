8a8cfe13026fa630329c45167e4092341c16ed1f
himanshu.patel <py.himanshu.patel@gmail.com>
2023-11-13 02:07:46 +0530

    Adapter update

--- design_patterns_golang/adapter/main.go
+++ design_patterns_golang/adapter/main.go
@@ -5,9 +5,16 @@ package main
 use command : `go run .`
 not `go run main.go`
 
-DEFINITION: Adapter Pattern
+---------------- DEFINITION: Adapter Pattern
 Adapter is a structural design pattern,
 which allows incompatible objects to collaborate.
+
+---------------- APPLICABILITY
+- Use the Adapter class when you want to use some existing class, 
+but its interface isn’t compatible with the rest of your code.
+
+- Use the pattern when you want to reuse several existing subclasses 
+that lack some common functionality that can’t be added to the superclass.
 
 */
 
