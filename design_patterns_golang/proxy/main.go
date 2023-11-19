6facc53ec20f1ec97a52b96f725648020ea580a3
himanshu.patel <py.himanshu.patel@gmail.com>
2023-11-19 14:46:50 +0530

    Proxy Update

--- design_patterns_golang/proxy/main.go
+++ design_patterns_golang/proxy/main.go
@@ -4,11 +4,34 @@ import "fmt"
 
 /*
 
+----------- DEFINITION: PROXY PATTERN
+
 Proxy is a structural design pattern that provides an object
 that acts as a substitute for a real service object used by a
 client. A proxy receives client requests, does some work
 (access control, caching, etc.) and then passes the request to
 a service object.
+
+----------- APPLICATION
+
+- Lazy initialization (virtual proxy). This is when you have a 
+heavyweight service object that wastes system resources by being 
+always up, even though you only need it from time to time.
+
+- Access control (protection proxy). This is when you want only 
+specific clients to be able to use the service object; for instance, 
+when your objects are crucial parts of an operating system and 
+clients are various launched applications (including malicious ones).
+
+- Logging requests (logging proxy). This is when you want to keep a 
+history of requests to the service object.
+
+- Caching request results (caching proxy). This is when you need to 
+cache results of client requests and manage the life cycle of this 
+cache, especially if results are quite large.
+
+- Smart reference. This is when you need to be able to dismiss a 
+heavyweight object once there are no clients that use it.
 
 */
 
